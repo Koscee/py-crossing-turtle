70bc26eb814e07e6a2c0706462fd2ead856e4c0d
Koscee <52253099+Koscee@users.noreply.github.com>
2023-08-24 14:06:17 +0800

    detect turtle collision with car

--- main.py
+++ main.py
@@ -18,5 +18,12 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    # create and move cars
     car_manager.create_car()
     car_manager.move_cars()
+    # detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
